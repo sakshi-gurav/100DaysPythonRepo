c7869b7180c6ff22fb91ef20994ce383d90f6bb1
Sakshi Gurav <guravsakshi01@gmail.com>
2024-06-14 00:09:26 +0530

    function

---
+++ Day5/function1.py
@@ -0,0 +1,9 @@
+def greet(name, greeting="Hello"):
+    return f"{greeting}, {name}!"
+
+print(greet("sakshi"))  # Output: Hello, Alice!
+print(greet("sam", "Hi"))  # Output: Hi, Bob!
+
+
+
+    #a function is agroup of statements performing a specific task
